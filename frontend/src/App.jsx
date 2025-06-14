dc475194ff6196b14063dcc1176250bb473d1797
KC1064 <kiron08072003@gmail.com>
2025-06-14 13:36:14 +0530

    Working on Onboarding Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
+import OnboardingPage from "./pages/OnboardingPage";
 import SignUp from "./pages/SignUp";
 import { Routes, Route } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/onboarding" element={<OnboardingPage />} />
     </Routes>
   );
 }
